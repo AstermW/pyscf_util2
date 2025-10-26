3acb539ba9b78230537c264868953bb0b5a87d84
NingZhang1 <ningzhang1024@gmail.com>
2025-10-27 00:02:18 +0800

    update cvs

--- examples/02-00-CVS-CASSCF.py
+++ examples/02-00-CVS-CASSCF.py
@@ -22,6 +22,8 @@ mol.build()
 mf = scf.RHF(mol)
 mf.kernel()
 
+init_guess = mf.mo_coeff.copy()
+
 norb = 9
 nelec = 12
 mymc2step = mcscf.CASSCF(mf, norb, nelec)
@@ -43,3 +45,30 @@ mymc2step.mc1step()
 
 mf.mo_coeff = mymc2step.mo_coeff
 tools.fcidump.from_scf(mf, "FCIDUMP_CO_C_Kedge_%s" % BASIS, tol=1e-10)
+
+# O edge #
+
+init_guess_O = init_guess.copy()
+init_guess_O[:, 0] = init_guess[:, 1]
+init_guess_O[:, 1] = init_guess[:, 0]
+
+mf.mo_coeff = init_guess_O
+mymc2step = mcscf.CASSCF(mf, norb, nelec)
+mymc2step.fcisolver = iCI(
+    mol=mol,
+    cmin=0.0,
+    state=[[0, 0, 1]],
+    tol=1e-12,
+    mo_coeff=mf.mo_coeff,
+    taskname="iCI0",
+    CVS=True,
+)
+mymc2step.fcisolver.config["segment"] = "0 0 1 4 4 0 0 0"
+mymc2step.fcisolver.config["selection"] = 1
+mymc2step.fcisolver.config["nvalelec"] = 12
+mymc2step.mc1step()
+
+# dump fcidump #
+
+mf.mo_coeff = mymc2step.mo_coeff
+tools.fcidump.from_scf(mf, "FCIDUMP_CO_O_Kedge_%s" % BASIS, tol=1e-10)
